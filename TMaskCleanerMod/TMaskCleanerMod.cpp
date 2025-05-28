3a75b1520131c5855ee71bf0781a3e5cfb1b056e
dtlnor <dtlnor@hotmail.com>
2025-05-28 18:49:52 +0900

    split into multi files

--- TMaskCleanerMod/TMaskCleanerMod.cpp
+++ TMaskCleanerMod/TMaskCleanerMod.cpp
@@ -1,71 +1,4 @@
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-#include <memory>
-#include <vector>
-#include "VapourSynth4.h"
-#include "VSHelper4.h"
-#include <string>
-#include <deque>
-#include <numeric>
-
-struct TMCData;
-
-typedef std::pair<int, int> Coordinates;
-typedef void (*Process_c_Ptr)(const VSFrame*, VSFrame*, int, const TMCData*, const VSAPI*);
-
-struct TMCData {
-    VSNode * node;
-    const VSVideoInfo * vi;
-    unsigned int length;
-    unsigned int thresh;
-    unsigned int fade;
-    Process_c_Ptr process_c_func;
-    const Coordinates* directions;
-    int dir_count;
-};
-
-template<typename pixel_t>
-inline bool is_white(pixel_t value, unsigned int thresh) {
-    return value >= thresh;
-}
-
-template<typename pixel_t>
-inline bool visited(int x, int y, int width, pixel_t *lookup, int bits) {
-    unsigned int normal_pos = y * width + x;
-    unsigned int byte_pos = normal_pos / bits;
-
-    return lookup[byte_pos] & (1 << (normal_pos - byte_pos * bits));
-}
-
-template<typename pixel_t>
-inline void visit(int x, int y, int width, pixel_t *lookup, int bits) {
-    unsigned int normal_pos = y * width + x;
-    unsigned int byte_pos = normal_pos / bits;
-
-    lookup[byte_pos] |= (1 << (normal_pos - byte_pos * bits));
-}
-
-constexpr Coordinates directions4[4] = { {0, -1}, {-1, 0}, {1, 0}, {0, 1} };
-constexpr Coordinates directions8[8] = {
-    {0, -1}, {-1, 0}, {1, 0}, {0, 1},
-    {-1, -1}, {1, -1}, {-1, 1}, {1, 1}
-};
-
-template<bool keep_less, typename pixel_t>
-void setProcessFunction(TMCData* d, int mode) {
-    switch (mode) {
-    case 0: d->process_c_func = &process_c<0, keep_less, pixel_t>; break;
-    case 1: d->process_c_func = &process_c<1, keep_less, pixel_t>; break;
-    case 2: d->process_c_func = &process_c<2, keep_less, pixel_t>; break;
-    case 3: d->process_c_func = &process_c<3, keep_less, pixel_t>; break;
-    case 4: d->process_c_func = &process_c<4, keep_less, pixel_t>; break;
-    case 5: d->process_c_func = &process_c<5, keep_less, pixel_t>; break;
-    case 6: d->process_c_func = &process_c<6, keep_less, pixel_t>; break;
-    case 7: d->process_c_func = &process_c<7, keep_less, pixel_t>; break;
-    case 8: d->process_c_func = &process_c<8, keep_less, pixel_t>; break;
-    default: throw std::string("mode must be in the range [0, 8].");
-    }
-}
+#include "shared.h"
 
 template<int filter_mode, bool keep_less, typename pixel_t>
 void process_c(const VSFrame * src, VSFrame * dst, int bits, const TMCData * d, const VSAPI * vsapi) {
@@ -238,12 +171,6 @@ static const VSFrame *VS_CC TMCGetFrame(int n, int activationReason, void *insta
     return nullptr;
 }
 
-static void VS_CC TMCFree(void *instanceData, VSCore *core, const VSAPI *vsapi) {
-    auto d{ static_cast<TMCData *>(instanceData) };
-    vsapi->freeNode(d->node);
-    delete d;
-}
-
 void VS_CC TMCCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi) {
     auto d{ std::make_unique<TMCData>() };
     int err{ 0 };
@@ -325,21 +252,6 @@ void VS_CC TMCCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, 
     }
 
     VSFilterDependency deps[] = {{d->node, rpStrictSpatial}};
-    vsapi->createVideoFilter(out, "TMaskCleanerMod", d->vi, TMCGetFrame, TMCFree, fmParallel, deps, 1, d.get(), core);
+    vsapi->createVideoFilter(out, "TMaskCleanerMod", d->vi, TMCGetFrame, FilterFree, fmParallel, deps, 1, d.get(), core);
     d.release();
-}
-
-VS_EXTERNAL_API(void) VapourSynthPluginInit2(VSPlugin *plugin, const VSPLUGINAPI *vspapi) {
-    vspapi->configPlugin("com.dtlnor.tmcm", "tmcm", "A really simple mask cleaning plugin for VapourSynth based on mt_hysteresis.", VS_MAKE_VERSION(1, 0), VAPOURSYNTH_API_VERSION, 0, plugin);
-
-    vspapi->registerFunction("TMaskCleanerMod",
-        "clip:vnode;"
-        "length:int:opt;"
-        "thresh:int:opt;"
-        "fade:int:opt;"
-        "connectivity:int:opt;"
-        "keepless:int:opt;"
-        "mode:int:opt;",
-        "clip:vnode;",
-        TMCCreate, nullptr, plugin);
 }
---
+++ TMaskCleanerMod/shared.cpp
@@ -0,0 +1,22 @@
+#include "shared.h"
+
+static void VS_CC FilterFree(void* instanceData, VSCore* core, const VSAPI* vsapi) {
+    auto d{ static_cast<TMCData*>(instanceData) };
+    vsapi->freeNode(d->node);
+    delete d;
+}
+
+VS_EXTERNAL_API(void) VapourSynthPluginInit2(VSPlugin* plugin, const VSPLUGINAPI* vspapi) {
+    vspapi->configPlugin("com.dtlnor.tmcm", "tmcm", "A really simple mask cleaning plugin for VapourSynth based on mt_hysteresis.", VS_MAKE_VERSION(1, 0), VAPOURSYNTH_API_VERSION, 0, plugin);
+
+    vspapi->registerFunction("TMaskCleanerMod",
+        "clip:vnode;"
+        "length:int:opt;"
+        "thresh:int:opt;"
+        "fade:int:opt;"
+        "connectivity:int:opt;"
+        "keepless:int:opt;"
+        "mode:int:opt;",
+        "clip:vnode;",
+        TMCCreate, nullptr, plugin);
+}
